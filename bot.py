475337ffa37bf731b9d5f5425cc87de3b8187856
Tiberio D A R Ferreira <tiberiusferreira@gmail.com>
2017-09-26 04:44:16 -0300

    add timer inside reply markup

--- bot.py
+++ bot.py
@@ -1,23 +1,32 @@
 from telegram import *
 from telegram.ext import *
+import time
+
 
 isOn = False
 currentUserName = ""
 currentUserId = ""
-
+currentUserChatId = ""
+time_left_to_off = 0
+turn_on_msg_id = ""
 keyboard_turn_on = [[InlineKeyboardButton("Ligar moedor", callback_data='turn_on')]]
-keyboard_turn_off = [[InlineKeyboardButton("Desligar moedor", callback_data='turn_off')]]
+
+
+def keyboard_turn_off():
+    return [[InlineKeyboardButton("Desligar o moedor " + str(int(time_left_to_off)), callback_data='turn_off')]]
 
 
 def is_in_use():
     return isOn
 
 
-def set_user(user_name, user_id):
-    global isOn, currentUserName, currentUserId
+def set_user(user_name, user_id, chat_id):
+    global isOn, currentUserName, currentUserId, time_left_to_off, currentUserChatId
     isOn = True
     currentUserId = user_id
     currentUserName = user_name
+    currentUserChatId = chat_id
+    time_left_to_off = 300
     return
 
 
@@ -31,14 +40,14 @@ def clear_user():
 
 def start(bot, update):
     if isOn and update.effective_user.id != currentUserId:
-        update.message.reply_text(currentUserName + ' já está usando a máquina. Por favor, aguarde.')
+        update.message.reply_text(currentUserName + ' já está usando a máquina. Por favor, aguarde ' + str(int(time_left_to_off)) + 'segundos.')
         return
 
     if isOn and update.effective_user.id == currentUserId:
-        reply_markup = InlineKeyboardMarkup(keyboard_turn_off)
+        reply_markup = InlineKeyboardMarkup(keyboard_turn_off())
         update.message.reply_text('Você já está usando o moedor.', reply_markup=reply_markup)
         return
-        
+
     reply_markup = InlineKeyboardMarkup(keyboard_turn_on)
     update.message.reply_text('O que você deseja fazer?', reply_markup=reply_markup)
 
@@ -59,12 +68,16 @@ def button(bot, update):
     query = update.callback_query
     if not is_in_use():
         if query.data == 'turn_on':
-            set_user(update.effective_user.first_name, update.effective_user.id)
-            reply_markup = InlineKeyboardMarkup(keyboard_turn_off)
+            global turn_on_msg_id, context
+            turn_on_msg_id = query.message.message_id
+            set_user(update.effective_user.first_name, update.effective_user.id, update.effective_chat.id)
+            reply_markup = InlineKeyboardMarkup(keyboard_turn_off())
+            context = query
             bot.edit_message_text(text="Todo uso do moedor será creditado à você durante os próximos 5 minutos ou até você clicar no botão abaixo",
                                   chat_id=query.message.chat_id,
                                   message_id=query.message.message_id,
                                   reply_markup=reply_markup)
+
             return
     if query.data == 'turn_off' and update.effective_user.id == currentUserId:
         clear_user()
@@ -93,12 +106,35 @@ updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CallbackQueryHandler(button))
 updater.dispatcher.add_handler(CommandHandler('help', help))
 
+
 def help(bot, update):
     update.message.reply_text("Digite /start para usar este bot.")
+
 
 # Start the Bot
 updater.start_polling()
 
-# Run the bot until the user presses Ctrl-C or the process receives SIGINT,
-# SIGTERM or SIGABRT
-updater.idle()
+
+while 1:
+    if isOn:
+        if time_left_to_off > 0:
+            last_time = time.time()
+            time.sleep(1)
+            time_now_millis = time.time()
+            if time_left_to_off-(time_now_millis-last_time) > 0:
+                time_left_to_off = time_left_to_off - (time_now_millis - last_time)
+                reply_markup = InlineKeyboardMarkup(keyboard_turn_off())
+                updater.bot.edit_message_reply_markup(
+                    text="Todo uso do moedor será creditado à você durante os próximos 5 minutos ou até você clicar no botão abaixo",
+                    chat_id=currentUserChatId,
+                    message_id=turn_on_msg_id,
+                    reply_markup=reply_markup)
+            else:
+                time_left_to_off = 0
+                clear_user()
+                updater.bot.send_message(
+                    text="O moedor foi desligado. O uso do moedor não esta mais atrelado a sua conta.",
+                    chat_id=currentUserChatId)
+
+
+
